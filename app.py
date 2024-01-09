5be841892e1c75df7dfd8a0ed465baf478b551cd
bpandey369 <pandeybhagat369@gmail.com>
2024-01-09 22:16:44 +0545

    gemini chatbot

---
+++ app.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+load_dotenv()  ## loading all the envvironment variables
+
+import streamlit as st
+import os
+import google.generativeai as genai
+genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+
+## function to load Gemini Pro model and get response
+model = genai.GenerativeModel('gemini-pro')
+def get_gemini_response(question):
+    response = model.generate_content(question)
+    return response.text
+
+## initialize our streamlit app
+
+st.set_page_config(page_title="Q&A Demo")
+st.header('Gemini LLM Applocation')
+input = st.text_input('Input: ',key='input')
+submit = st.button('Ask the question')
+
+## when submit is clicked
+if submit:
+    response = get_gemini_response(input)
+    st.subheader('The Response is')
+    st.write(response)
